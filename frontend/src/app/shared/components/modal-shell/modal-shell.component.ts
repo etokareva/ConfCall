5db12bf8e4e253e9af2bca8ec5fbbcd4541fbfc3
Ekaterina Tokareva <e.tokareva@big3.ru>
2026-07-10 16:16:17 +0300

    fix(frontend): centralize modal focus handling

--- frontend/src/app/shared/components/modal-shell/modal-shell.component.ts
+++ frontend/src/app/shared/components/modal-shell/modal-shell.component.ts
@@ -1,11 +1,14 @@
 import { CommonModule } from "@angular/common";
 import { Component, EventEmitter, Input, Output } from "@angular/core";
+import { A11yModule } from "@angular/cdk/a11y";
 import { TranslatePipe } from "../../../core/i18n/translate.pipe";
+
+let modalShellId = 0;
 
 @Component({
   selector: "ccs-modal-shell",
   standalone: true,
-  imports: [CommonModule, TranslatePipe],
+  imports: [A11yModule, CommonModule, TranslatePipe],
   templateUrl: "./modal-shell.component.html",
   styleUrl: "./modal-shell.component.scss",
 })
@@ -13,7 +16,8 @@ export class ModalShellComponent {
   @Input() eyebrow = "";
   @Input({ required: true }) title = "";
   @Input() subtitle = "";
-  @Input() titleId = "";
+  @Input() titleId = `modal-shell-title-${++modalShellId}`;
+  @Input() describedBy = "";
   @Input() closeLabel = "common.close";
   @Input() showClose = true;
   @Output() readonly close = new EventEmitter<void>();
